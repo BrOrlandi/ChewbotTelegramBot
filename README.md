f5b7677b43603acb9c94cceda86674dbd385956b
Bruno Orlandi <brorlandi@gmail.com>
2015-12-08 17:09:21 -0200

    First commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chewbot for Telegram
+Just a simple bot for Telegram. Use the BotFather to generate an API Token for use this bot with Telegram.
+> "RRRAARRWHHGWWR.", Chewbacca
+
+## How to use
+Talks to Chewbacca and he will always answer you with a voice message.
+
+## Improvements
+- [ ] Command /gif answer with a random Chewbacca Gif using Giphy API.
---
+++ bot.js
@@ -0,0 +1,25 @@
+var TelegramBot = require('node-telegram-bot-api');
+
+var token = process.env.TOKEN || "";
+
+if(token === ""){
+    console.log("Use the env var 'TOKEN' to set your Telegram Bot API token.")
+    return ;
+}
+
+// Setup polling way
+var bot = new TelegramBot(token, {polling: true});
+
+// Any kind of message
+bot.on('message', function (msg) {
+  var chatId = msg.chat.id;
+  //console.log("From: "+msg.from.first_name + " " + msg.from.last_name+ " : "+ msg.text);
+  bot.sendChatAction(chatId,'record_audio');
+
+  var rand = Math.floor(Math.random()*18)+1;
+  var audio = 'sounds/s'+rand+'.ogg';
+  //console.log("Sending: " + audio);
+  bot.sendVoice(chatId, audio);
+});
+
+console.log("Chewbacca bot running...");
