1fefd8b7ad77774383fcdd5d02f55feef68b0f2f
Bruno Orlandi <brorlandi@gmail.com>
2015-12-08 18:24:49 -0200

    Makes bot reply a user. Nice for group messages.

--- bot.js
+++ bot.js
@@ -12,14 +12,16 @@ var bot = new TelegramBot(token, {polling: true});
 
 // Any kind of message
 bot.on('message', function (msg) {
+
   var chatId = msg.chat.id;
-  //console.log("From: "+msg.from.first_name + " " + msg.from.last_name+ " : "+ msg.text);
+  console.log("From: "+msg.from.first_name + " " + msg.from.last_name+ " : "+ msg.text);
   bot.sendChatAction(chatId,'record_audio');
 
   var rand = Math.floor(Math.random()*18)+1;
   var audio = 'sounds/s'+rand+'.ogg';
-  //console.log("Sending: " + audio);
-  bot.sendVoice(chatId, audio);
+  console.log("Sending: " + audio);
+  var replyTo = msg.message_id;
+  bot.sendVoice(chatId, audio, {reply_to_message_id: replyTo});
 });
 
 console.log("Chewbacca bot running...");
