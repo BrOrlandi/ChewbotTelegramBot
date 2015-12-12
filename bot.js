a79bb485867d3184f2f5dd5ea265ce676a597ec9
Bruno Orlandi <brorlandi@gmail.com>
2015-12-12 20:07:09 -0200

    Changed sounds to MP3, Gif command WIP.

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 var TelegramBot = require('node-telegram-bot-api');
+var giphy = require('giphy-api')();
 
 var token = process.env.TOKEN || "";
 
@@ -10,18 +11,46 @@ if(token === ""){
 // Setup polling way
 var bot = new TelegramBot(token, {polling: true});
 
+
+bot.onText(/\/start/, function (msg, match) {
+  var fromId = msg.from.id;
+  bot.sendMessage(fromId, "RRRAARRWHHGWWR!");
+});
+
+bot.onText(/\/gif/, function (msg, match) {
+  var chatId = msg.chat.id;
+  var replyTo = msg.message_id;
+  bot.sendChatAction(chatId,'upload_photo');
+
+  giphy.translate('chewbacca').then((res)=>{
+    var gif = res.data.images.fixed_height.url;
+    console.log("Sending gif "+ gif +" to: "+msg.from.first_name + " " + msg.from.last_name+ " : ");
+    //console.log(res.data.images.fixed_height.url);
+    bot.sendPhoto(chatId,gif,{reply_to_message_id: replyTo});
+    
+  });
+});
+
+
+
 // Any kind of message
 bot.on('message', function (msg) {
+   if(msg.text.startsWith('/'))
+    return;
 
   var chatId = msg.chat.id;
   console.log("From: "+msg.from.first_name + " " + msg.from.last_name+ " : "+ msg.text);
   bot.sendChatAction(chatId,'record_audio');
 
   var rand = Math.floor(Math.random()*18)+1;
-  var audio = 'sounds/s'+rand+'.ogg';
+  var audio = 'sounds/mp3/s'+rand+'.mp3';
   console.log("Sending: " + audio);
   var replyTo = msg.message_id;
-  bot.sendVoice(chatId, audio, {reply_to_message_id: replyTo});
+  var randTime = Math.floor(Math.random()*1000)+2000;
+  console.log("time: " + randTime)
+  setTimeout(()=>{
+    bot.sendVoice(chatId, audio, {reply_to_message_id: replyTo});
+  },randTime);
 });
 
 console.log("Chewbacca bot running...");
